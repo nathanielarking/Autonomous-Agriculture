31686f28824a1fbbfb13e6d00549079d6a8e544c
nathanielarking <nathanielarking@gmail.com>
2022-02-12 23:13:59 -0800

    Added ability to add new harvest entries

--- webapp/pages/entry/entry.py
+++ webapp/pages/entry/entry.py
@@ -12,20 +12,17 @@ entry_blueprint = Blueprint('entry_blueprint', __name__,
     template_folder='templates',
     static_folder='static')
 
-#Route to view the harvest graphs
+#Route to add new planting entries graphs
 @entry_blueprint.route('/entry/planting', methods=['GET', 'POST'])
 @login_required
 def entry_planting():
 
-    #Get the names and variety of plants
+    #Get the names of plants
     names = []
-    variety = 0
     with Session(engine) as session:
         plants = session.query(Plant).filter_by(active=1).all()
-            
         for plant in plants:
             names.append(plant.name)
-            variety += 1
 
     current_day = date.today().strftime("%Y-%m-%d")
 
@@ -37,17 +34,46 @@ def entry_planting():
         entry_date = datetime.strptime(entry_date, "%Y-%m-%d")
         description = request.form.get('description')
 
-        with Session(engine) as session:
-            plant = session.query(Plant).filter_by(name=name).one()
-            entry = PlantingEntry( Plant_id=plant.id, Plant=plant, date=entry_date, description=description)
-            session.add(entry)
-            session.commit()
+        if description is not None:
+            with Session(engine) as session:
+                plant = session.query(Plant).filter_by(name=name).one()
+                entry = PlantingEntry(Plant_id=plant.id, Plant=plant, date=entry_date, description=description)
+                session.add(entry)
+                session.commit()
 
     return render_template('entry/planting.html', user=current_user, palette=palette, names=names, current_day=current_day)
 
-#Route to view the harvest data
-@entry_blueprint.route('/entry/harvest')
+#Route to add new harvest entries
+@entry_blueprint.route('/entry/harvest', methods=['GET', 'POST'])
 @login_required
 def entry_harvest():
 
-    return render_template('entry/harvest.html', user=current_user, palette=palette)
+    #Get the names of plants
+    names = []
+    with Session(engine) as session:
+        plants = session.query(Plant).filter_by(active=1).all()   
+        for plant in plants:
+            names.append(plant.name)
+
+    current_day = date.today().strftime("%Y-%m-%d")
+
+    #Add new planting to the database
+    if request.method == "POST":
+        #Assign variables from request
+        name = request.form.get('name')
+        entry_date = request.form.get('date')
+        entry_date = datetime.strptime(entry_date, "%Y-%m-%d")
+        mass = request.form.get('mass')
+
+        if mass is not None:
+            with Session(engine) as session:
+                plant = session.query(Plant).filter_by(name=name).one()
+                print(plant.id)
+                print(plant)
+                print(entry_date)
+                print(mass)
+                entry = HarvestEntry(Plant_id=plant.id, Plant=plant, date=entry_date, mass=mass)
+                session.add(entry)
+                session.commit()
+
+    return render_template('entry/harvest.html', user=current_user, palette=palette, names=names, current_day=current_day)
